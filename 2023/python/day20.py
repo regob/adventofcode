cbd9b4bdb0c037291923f136eb632bc9b90da561
regob <rego97@gmail.com>
2023-12-24 12:30:03 +0100

    Facepalm

--- 2023/python/day20.py
+++ 2023/python/day20.py
@@ -1,14 +1,8 @@
 import re
-from ast import literal_eval
-from functools import reduce, cache
-from itertools import product
-from string import digits
-from operator import mul
-from collections import Counter, namedtuple, deque
+from collections import deque
 import math
 
 INPUT_FILE = 'input/2023_20.txt'
-# INPUT_FILE = 'input/2023_20_test.txt'
 
 with open(INPUT_FILE) as fp:
     txt = fp.read().strip()
@@ -18,22 +12,30 @@ pat = '([%&])?([a-zA-Z]+) -> (.*)'
 
 nxt = {}
 node_kind = {}
-conj_inputs = {}
+inputs = {}
 state = {}
 
-for line in entries:
-    kind, name, targets = re.match(pat, line).groups()
-    targets = [x.strip() for x in targets.split(',')]
-    print(kind, name, targets)
-    nxt[name] = targets
-    node_kind[name] = kind
-    state[name] = [] if kind == '&' else False
 
-for k, targets in nxt.items():
-    for tar in targets:
-        if node_kind.get(tar, -1) == '&':
-            conj_inputs.setdefault(tar, []).append(k)
-            state[tar].append(False)
+def init_state():
+    for d in (nxt, node_kind, inputs, state):
+        d.clear()
+
+    for line in entries:
+        kind, name, targets = re.match(pat, line).groups()
+        targets = [x.strip() for x in targets.split(',')]
+
+        nxt[name] = targets
+        node_kind[name] = kind
+        state[name] = [] if kind == '&' else False
+
+    for k, targets in nxt.items():
+        for tar in targets:
+            inputs.setdefault(tar, []).append(k)
+            if node_kind.get(tar, -1) == '&':
+                state[tar].append(False)
+
+########################################
+# part 1
 
 
 def step(node, level, prev):
@@ -52,7 +54,7 @@ def step(node, level, prev):
         return [(s, level) for s in nexts]
 
     assert kind == '&', kind
-    prev_idx = conj_inputs[node].index(prev)
+    prev_idx = inputs[node].index(prev)
     assert prev_idx >= 0
     state[node][prev_idx] = level
 
@@ -62,53 +64,46 @@ def step(node, level, prev):
 
 N = 1000
 total = {True: 0, False: 0}
+init_state()
 
-# part 1
-# for t in range(N):
-#     print(t)
+for t in range(N):
+    print(t)
 
-#     nodes = deque([('broadcaster', False, 'button')])
-#     while len(nodes):
-#         ns, level, prev = nodes.popleft()
-#         # print(prev, f'-{level}' '->', ns)
-#         total[level] += 1
-#         next_states = step(ns, level, prev)
-#         nodes.extend([(s, lev, ns) for s, lev in next_states])
+    nodes = deque([('broadcaster', False, 'button')])
+    while len(nodes):
+        ns, level, prev = nodes.popleft()
+        # print(prev, f'-{level}' '->', ns)
+        total[level] += 1
+        next_states = step(ns, level, prev)
+        nodes.extend([(s, lev, ns) for s, lev in next_states])
 
-# print(total)
-# print(total[True] * total[False])
-
-
-def save_state():
-    s = [(k, tuple(v) if isinstance(v, list) else v) for k, v in state.items()]
-    s.sort()
-    return tuple(s)
+print('part1', total[True] * total[False])
 
 
+########################################
 # part 2
+
+init_state()
+final_nodes = inputs[inputs['rx'][0]]
+cycle = {}
+
 t = 1
-seen_states = set(save_state())
 while True:
     nodes = deque([('broadcaster', False, 'button')])
-    rx_cnt = 0
 
     while len(nodes):
         ns, level, prev = nodes.popleft()
-        if ns == 'rx':
-            if level:
-                rx_cnt = -1
-            elif rx_cnt >= 0:
-                rx_cnt += 1
+        if prev in final_nodes and level:
+            print(t, prev)
+            if prev not in cycle:
+                cycle[prev] = t
 
         next_states = step(ns, level, prev)
         nodes.extend([(s, lev, ns) for s, lev in next_states])
 
-    if rx_cnt == 1:
-        print('part 2', t)
-
-    st = save_state()
-    if st in seen_states:
-        print(f'Loop at {t}', st)
-    seen_states.add(st)
-    # print(t, state['vf'])
+    if len(cycle) == len(final_nodes):
+        break
     t += 1
+
+cycles = list(cycle.values())
+print('part2', math.lcm(*cycles))
